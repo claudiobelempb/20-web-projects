60cc778da043ca151a9ed19e10a8e56cd5d279dc
claudiobelempb <claudio.c.lima@hotmail.com>
2021-07-13 18:04:37 -0300

    Check Required & Refactor

--- form-validator/assets/js/script.js
+++ form-validator/assets/js/script.js
@@ -19,29 +19,23 @@ function isValidEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+// check required fields
+function checkRequired(inputArr){
+  inputArr.forEach(function(input) {
+    if(input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required!`);
+    }else{
+      showSuccess(input);
+    }
+  });
+}
+// get field name
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
 
+// Event listeners
 elForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  if (elUserName.value === '') {
-    showError(elUserName, 'Username is required!');
-  } else {
-    showSuccess(elUserName);
-  };
-  if (elEmail.value === '') {
-    showError(elEmail, 'Email is required!');
-  } else if (!isValidEmail(elEmail.value)) {
-    showError(elEmail, 'Email is not valid!');
-  } else {
-    showSuccess(elEmail);
-  };
-  if (elPassword.value === '') {
-    showError(elPassword, 'Password is required!');
-  } else {
-    showSuccess(elPassword);
-  };
-  if (elPassword2.value === '') {
-    showError(elPassword2, 'Password is required!');
-  } else {
-    showSuccess(elPassword2);
-  };
+  checkRequired([elUserName, elEmail, elPassword, elPassword2]);
 });
